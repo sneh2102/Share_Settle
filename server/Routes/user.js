cb6e54210538775dd9804dd4870d1aac7f5c7478
Priyatam Reddy Somagattu <pr889078@dal.ca>
2023-10-27 20:37:46 -0300

    fix: moved the user routes to the correct position

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { signupUser, loginUser ,forgotPassUser, resetPassUser} = require('../Controller/userController');
+const { signupUser, loginUser ,forgotPassUser, resetPassUser, changeUsername, changePassword} = require('../Controller/userController');
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post('/login', loginUser);
 router.post('/signup', signupUser);
 router.post('/forgotpass', forgotPassUser)
 router.post('/reset-password/:id/:token', resetPassUser)
+router.post('/changeUsername', changeUsername)
+router.post('/changePassword', changePassword)
+
 
 module.exports = router;
